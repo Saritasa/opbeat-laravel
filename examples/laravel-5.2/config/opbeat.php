1ae34020354e2d7997c2a4b3e9dbb465a3094d15
Sergey Populov <sergey@saritasa.com>
2016-11-08 15:22:49 -0800

    Move app parameters from hardcoded config to .env.example

--- examples/laravel-5.2/config/opbeat.php
+++ examples/laravel-5.2/config/opbeat.php
@@ -11,9 +11,9 @@ return [
     |
     */
 
-    'app_id' => env('OPBEAT_APP_ID', 'bf948ea29c'),
-    'organization_id' => env('OPBEAT_ORGANIZATION_ID', '4c394af520484784a993cc8977e7c0ce'),
-    'access_token' => env('OPBEAT_ACCESS_TOKEN', '3a21ef7091b4082d12b0280f81c7b02c74a657d5'),
+    'app_id' => env('OPBEAT_APP_ID', ''),
+    'organization_id' => env('OPBEAT_ORGANIZATION_ID', ''),
+    'access_token' => env('OPBEAT_ACCESS_TOKEN', ''),
     'enable_exception_handler' => false,
     'enable_error_handler' => false
 ];
